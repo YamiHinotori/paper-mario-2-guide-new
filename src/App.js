83496b59344d2a8ba944143c6f786bb540d41f67
YamiHinotori <ckrell@kabelmail.de>
2025-03-15 20:13:04 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+
+function App() {
+    const [isChecked, setIsChecked] = useState(false);
+
+    // Beim Laden der App den gespeicherten Zustand abrufen
+    useEffect(() => {
+        const savedState = localStorage.getItem("checkboxState") === "true";
+        setIsChecked(savedState);
+    }, []);
+
+    // Zustand speichern, wenn Checkbox geändert wird
+    const handleCheckboxChange = () => {
+        setIsChecked(!isChecked);
+        localStorage.setItem("checkboxState", !isChecked);
+    };
+
+    return (
+        <div style={{ textAlign: "center", marginTop: "50px" }}>
+            <h1>Meine React-PWA</h1>
+            <label>
+                <input type="checkbox" checked={isChecked} onChange={handleCheckboxChange} />
+                Merke mich!
+            </label>
+        </div>
+    );
+}
+
+export default App;
